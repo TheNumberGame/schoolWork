6a2472a03c3ebada653bcd7d9eedf3e9a15efddb
TheNumberGame <31251497+TheNumberGame@users.noreply.github.com>
2018-01-31 09:31:31 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 app.secret_key=os.environ["SECRET_KEY"];
 
 overAll = 0
+correct = ["any", "Yes", "end"]
 
 @app.route('/', methods=['GET', 'POST'])
 def renderMain():
@@ -22,9 +23,14 @@ def renderOutOf():
 
 @app.route('/Q', methods=['GET', 'POST'])
 def editCookie():
-  global overAll
-  session['question'] = request.form['question']
-  #If statement creating errors. Causes session['question'] to not update for whatever reason.    
+  global overAll, correct
+  str = request.form['question'][0]
+  num = request.form['question'][1]
+  if num == 1:
+    session["question"] = 1
+  elif num == 2 and str == "Yes":
+    session["question"] = 2
+  
   if int(session['question']) >= overAll:
     overAll = int(session['question'])
     session['allTime'] = overAll
