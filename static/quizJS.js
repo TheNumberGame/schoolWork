decba9e27eb23337c27e0abb571c1f26bece2ec7
TheNumberGame <31251497+TheNumberGame@users.noreply.github.com>
2018-01-31 09:05:21 -0800

    Update quizJS.js

--- static/quizJS.js
+++ static/quizJS.js
@@ -1,22 +1,24 @@
 $(document).ready(function(){
 	$(".a1").click(function(){
-		$.post("/Q",{ question: 1});
+		$.post("/Q",{ question: "Yes"});
 		$(".a1").hide(function(){
 			$("#pr1").show(function(){
 				$("#q2").show();
 				$(".a2").show(function(){	
 					$("#n2").click(function(){
-						$.post("/Q",{ question: 2});
+						$.post("/Q",{ question: "No"});
 						$(".a2").hide();
 						$("#pr21").show(function(){
 							$("#q3").show();
 							$(".a3").show(function(){	
 								$("#n3").click(function(){
+									$.post("/Q",{ question: "No"});
 									$(".a3").hide();
 									$("#pr31").show();
 									$("#b1").show();
 								});
 								$("#y3").click(function(){
+									$.post("/Q",{ question: "Yes"});
 									$(".a3").hide();
 									$("#pr3").show();
 									$("#b1").show();
@@ -25,16 +27,19 @@ $(document).ready(function(){
 						});
 					});
 					$("#y2").click(function(){
+						$.post("/Q",{ question: "Yes"});
 						$(".a2").hide();
 						$("#pr2").show(function(){
 							$("#q3").show();
 							$(".a3").show(function(){	
 								$("#n3").click(function(){
+									$.post("/Q",{ question: "No"});
 									$(".a3").hide();
 									$("#pr31").show();
 									$("#b1").show();
 								});
 								$("#y3").click(function(){
+									$.post("/Q",{ question: "Yes"});
 									$(".a3").hide();
 									$("#pr3").show();
 									$("#b1").show();
