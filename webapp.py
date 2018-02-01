595820a4f8346ff46ac9eca87fde1c6dffb1b33a
TheNumberGame <31251497+TheNumberGame@users.noreply.github.com>
2018-02-01 09:31:52 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask, url_for, render_template, request
 from flask import redirect
 from flask import session
-from random import randrange
+import random
 
 app = Flask(__name__)
 
